08885008a562241b4ef09bd037e8a6279f4150e9
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2024-02-17 14:21:54 +0100

    add cors configuration

---
+++ src/main/java/com/example/aftas_back/config/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.example.aftas_back.config;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class CorsConfig implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+        httpResponse.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+        httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        httpResponse.setHeader("Access-Control-Max-Age", "3600");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
