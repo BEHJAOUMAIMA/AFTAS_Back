a98ca78f2e822db4f7d3a536be2d583fe0c5e3b3
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2024-02-17 12:39:30 +0100

    init project

---
+++ src/main/java/com/example/aftas_back/AftasBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.aftas_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftasBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AftasBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/aftas_back/AftasBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aftas_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AftasBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
