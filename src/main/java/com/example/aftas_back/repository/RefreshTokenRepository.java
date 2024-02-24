9b6c888a0d476417ba7f9839b75c914f2f4547dd
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2024-02-24 18:41:49 +0100

    add RefreshToken Repository

---
+++ src/main/java/com/example/aftas_back/repository/RefreshTokenRepository.java
@@ -0,0 +1,12 @@
+package com.example.aftas_back.repository;
+
+import com.example.aftas_back.domain.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByToken(String token);
+}
