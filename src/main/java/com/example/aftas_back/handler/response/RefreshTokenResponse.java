5cb8edb5946024e66001492d9e723e2cec440025
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2024-02-24 18:53:08 +0100

    add RefreshTokenResponse

---
+++ src/main/java/com/example/aftas_back/handler/response/RefreshTokenResponse.java
@@ -0,0 +1,21 @@
+package com.example.aftas_back.handler.response;
+
+import com.example.aftas_back.domain.enums.TokenType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshTokenResponse {
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+    @JsonProperty("token_type")
+    private String tokenType = TokenType.BEARER.name();
+}
