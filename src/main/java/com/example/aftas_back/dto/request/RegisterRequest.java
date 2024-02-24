9f554335b5b8dbe1771401c0e3d7ecd078daf57d
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2024-02-24 18:51:27 +0100

    add Register Request & authenticationResponse

--- src/main/java/com/example/aftas_back/dto/request/RegisterRequest.java
+++ src/main/java/com/example/aftas_back/dto/request/RegisterRequest.java
@@ -1,5 +1,10 @@
 package com.example.aftas_back.dto.request;
 
+import com.example.aftas_back.domain.enums.IdentityDocumentType;
+import com.example.aftas_back.domain.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,13 +15,35 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
+    @NotBlank(message = "firstname is required")
+    @NotNull
+    private String name;
 
-    private String firstname;
+    @NotBlank(message = "lastname is required")
+    @NotNull
+    private String familyName;
 
-    private String lastname;
+    @NotBlank(message = "nationality is required")
+    @NotNull
+    private String nationality;
 
+    @NotBlank(message = "identityDocumentType is required")
+    @NotNull
+    private IdentityDocumentType identityDocumentType;
+
+    @NotBlank(message = "identityNumber is required")
+    @NotNull
+    private String identityNumber;
+
+    @NotBlank(message = "email is required")
+    @Email(message = "email format is not valid")
     private String email;
 
+    @NotBlank(message = "password is required")
+    @NotNull
     private String password;
+
+    @NotNull
+    private Role role;
 
 }
